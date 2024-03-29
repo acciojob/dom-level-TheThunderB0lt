a58cb885c7584b1c4575d8479a821cd2218adaed
NIKITH KUMAR N <nikithkumarniki160698@gmail.com>
2024-03-29 22:29:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+var element = document.getElementById('level');
+var parent = element.parentNode;
+var counter = 0;
+while (element != null) {
+    counter++;
+    element = element.parentNode;
+}
+alert('The level of the element is: ' + counter);
